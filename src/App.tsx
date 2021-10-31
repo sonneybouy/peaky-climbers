c1643aea66ae6a4ef415edba147c74abfd1b9d33
Sonney Patel <sonney.patel@gmail.com>
2021-10-31 19:53:16 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import picture from './assets/logo800.jpeg';
+import TopNavBar from './components/TopNavBar';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={picture} className="App-logo" alt="logo"/>
+            </header>
+            <TopNavBar/>
+            <header className="App-body">
+                Coming Soon!
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/TopNavBar.tsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import './topnavbar.css';
+import smallLogo from './../assets/logo69.png';
+
+
+function TopNavBar() {
+
+    const [scrolled, setScrolled] = useState(false);
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 900) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    });
+
+    let navBarClasses = ['navbar'];
+
+    if (scrolled) {
+        navBarClasses.push('scrolled');
+    }
+
+    return (
+        <header className={navBarClasses.join(" ")}>
+            <div className="logo">
+                <img src={smallLogo} className="logo"/>
+            </div>
+            <nav className="navigation">
+                <a href={"https://ketwork.github.io/profile/schedule.html"}>Resets</a>
+            </nav>
+        </header>
+    );
+}
+
+export default TopNavBar;
